2bf0a51f79f0693d452f385ba4bcb32fc04b1156
Ria De <riadernpur@gmail.com>
2023-12-01 00:37:04 -0600

    updated index.js file

--- index.js
+++ index.js
@@ -19,36 +19,6 @@ app.use(express.json());
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-/** Routes
-app.get('/', function (req, res) {
-	console.log("Sent as a get request");
 
-	res.render('index', { title: "BookHub"})
-})
 
-app.get('/users/:id', function (req, res) {
-	//Getting id parameter
-	var id = req.params.id;
-	console.log("Sent as a get request");
-	res.render('home', { title: "Routing in Action!", user_id : id})
-})
-
-app.get('/contact', function (req, res) {
-
-	res.render( 'contact', {title : "Contact Page"})
- })
-
- app.post('/submit', function (req, res) {
-	//Getting body parameters
-	var data = req.body;
-	var firstName = data.fname;
-	var lastName = data.lname;
-	var id = data.id;
-
-	console.log("Sent as a post request");
-	console.log(firstName + " " + lastName + " " + id );
-	res.render( 'contact', {title : "Contact Page"})
- })
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))**/
 
