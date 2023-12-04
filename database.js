e0223b620a522c099816ce9e89d69efd51897ae4
Ria De <riadernpur@gmail.com>
2023-12-03 21:50:25 -0600

    Fixed create Account

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 
-
-// data base route
+// Deliverable 5
+// file named database.js, contains all SQL queries
 
 var sqlite3 = require('sqlite3').verbose(); // npm install sqlite3
 
@@ -17,30 +17,73 @@ let db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE, (err) => {
     }
 });
 
-//Get users 
+// data base routes
 
-let getAllUsers = (res) => {
-  var getAll = 'SELECT username, firstName FROM user';
-  db.all(getAll, function (err, rows) {
-      if (err) {
-          throw err;
-      }
-      
-      // Send the retrieved data back to the client
-      res.render('index',(rows));
-  });
+// Query #1
+// CREATE operation
+// Function to create user in the database
+let createUser = (userName, password, firstName, lastName, email, res) => {
+    var createNewUser= 'INSERT INTO user (userName, password, firstName, lastName, email, userId) VALUES (?, ?, ?, ?, ?, ?)';
+    var userId = generateUUID()
+    var params= [userName, password, firstName, lastName, email, userId];
+    if (!userName || userName == '') {
+        res.render('error', {
+            message: "userName is Empty or undefined"
+        })
+    }
+    if (!password || password == '') {
+        res.render('error', {
+            message: "password is Empty or undefined"
+        })
+    }
+    if (!firstName || firstName == '') {
+        res.render('error', {
+            message: "firstName is Empty or undefined"
+        })
+    }
+    if (!lastName || lastName == '') {
+        res.render('error', {
+            message: "lastName is Empty or undefined"
+        })
+    }
+    if (!email || email == '') {
+        res.render('error', {
+            message: "email is Empty or undefined"
+        })
+    }
+    
+    db.run(createNewUser, params, function(err) {
+        if (err) {
+            res.render('error', {
+                message: err.message
+            })
+        } else {
+           res.render('accountCreated', {})
+        }
+    });
 };
 
-
+// Query #2
+// RETRIEVE operation
+// Get a user and validate password
 let getUserByUserName = (username, password, res) => {
     var params = [username]
     var getAll = 'SELECT firstName, lastName, email, userId, userName, password FROM user WHERE userName=?';
-    
+    if (!username || username == '') {
+        res.render('error', {
+            message: "userName is Empty or undefined"
+        })
+    }
+    if (!password || password == '') {
+        res.render('error', {
+            message: "password is Empty or undefined"
+        })
+    }
     db.get(getAll, params, function (err, user) {
-        console.log("users: ", user)
-        console.log("password: ", password)
         if (err) {
-            throw err;
+            res.render('error', {
+                message: err.message
+            })
         }
         if (!user) {
             res.render('userNotFound',({
@@ -56,56 +99,44 @@ let getUserByUserName = (username, password, res) => {
             res.render('user',(user));
         }
     });
-  };
-
-// function getUserByUserName(username, password) {
-//     return new Promise((resolve, reject) => {          // return new Promise here <---
-//         var params = [username]
-//         var sql = 'SELECT firstName, lastName, email, userId, userName, password FROM user WHERE userName=?';
-      
-//         return db.run(sql, params, function (err, res) { // .run <----
-//             if (err) {
-//                 console.error("DB Error: getUserByUserName failed: ", err.message);
-//                 return reject(err.message);
-//             }
-//             console.log(res)
-//             return resolve(res);
-//         });
-//     });
-//   }
-
-
-// Function to create user in the database
-let createUser = (userName, password, firstName, lastName, email, res) => {
-    var createNewUser= 'INSERT INTO user (userName, password, firstName, lastName, email) VALUES (?, ?, ?, ?, ?)';
-    var params= [userName, password, firstName, lastName, email];
-    
-    db.run(createNewUser, params, function(err) {
-        if (err) {
-           return console.log (err.message);
-            
-        } else {
-           return console.log ('Account created successfully!');
-        }
-    });
 };
 
+// Query #3
+// RETRIEVE operation
+// Get a list of all users
+let getAllUsers = (res) => {
+  var getAll = 'SELECT username, firstName FROM user';
+  db.all(getAll, function (err, rows) {
+      if (err) {
+        res.render('error', {
+            message: err.message
+        })
+      }
+      
+      // Send the retrieved data back to the client
+      res.render('index',(rows));
+  });
+};
 
-// update user information mail and password 
+// Query #4
+// UPDATE operation
+// update user information - email
 let updateMail = (email, res) => {
   const updateQuery = 'UPDATE user SET  email=? WHERE id=?';
   db.run(updateQuery, [email], (err) => {
       if (err) {
-          console.error(err.message);
-          return('Error updating user.');
+        res.render('error', {
+            message: err.message
+        })
       } else {
           return('User updated successfully!');
       }
   });
 };
 
-
-//deleteaccount
+// Query #5
+// DELETE operation
+// delete a user entry
 let deleteAccount = (userId, res) => {
   var deleteA ='DELETE FROM user WHERE userId=?';
 
@@ -113,15 +144,30 @@ let deleteAccount = (userId, res) => {
 
   db.run(deleteA, params, (err) => {
     if (err) {
-      console.error(err.message);
-      return('Error deleting account.');
+        res.render('error', {
+            message: err.message
+        })
   } else {
       return('Account deleted successfully!');
   }
  }); 
 };
 
-
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 module.exports = { getAllUsers, createUser, updateMail, deleteAccount, getUserByUserName};
 
--- index.js
+++ index.js
@@ -10,178 +10,98 @@ const port = 3000
 /**To serve static files such as images, CSS files, and JavaScript files, create a folders
 * and include the below statement.  The below statement assumes that I have a folder named assets
 **/
+
+// Deliverable 4
+// Middlewares
+
+// Middleware is implemented for static files
 app.use(express.static('assets'))
 
-
-// view engine setup
+// Middleware is implemented for viewing template engine
 app.set("view engine", "hbs");
-
 // allows our application to use .html extension | *Create a views folder and add your HTML documents
 app.engine('html', require('hbs').__express);
 
-
+// Middleware is implemented to parse json data
 // parse application/json
 app.use(express.json());
-
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 
+// Application has minimum 5 routes implemented
 
-
-/*
-// Function to welcome a user
-function welcomeUser(userName) {
-    console.log("Welcome " + userName + " ! Enjoy your literary journey.");
- }
- //Define the variable
- var user = "Shuvrajit";
- 
- 
- //call the function
- welcomeUser(user);
- 
- 
- 
- 
- // Function to search for a book
- function searchBook(bookTitle) {
-    console.log("Searching for " + bookTitle);
- }
- //Define the variable
- var bookName = "The God of Small Things";
- 
- 
- //call the function
- searchBook(bookName);
- 
- 
- 
- 
- // Function to add a review
- function addReview(bookTitle, review) {
-    console.log("Adding review for " + bookTitle + " : " + review);
- }
- //Define the variable
- var comment = "The Characters are just so realistic and full of life along with the beautiful poetic writing style with the wonderful atmosphere built in just for the perfect book. Esthappen, Rahel and Ammu are just my absolute favourites and I love them by all means.";
- 
- 
- //call the function
- addReview(bookName,comment);
- 
- 
- // Function to join a book club or fandom
- function joinCommunity(userName, communityType) {
-    console.log(userName + " has joined the " + communityType);
- }
- //Define the variable
- var community  = "Fiction Lover";
- 
- 
- //call the function
- joinCommunity(user, community);*/
- 
-
-// Routes
-app.get('/', (req, res) => {
-   dbOperations.getAllUsers(res)
-   //res.render('index.hbs', { title: 'BookHub' });
- });
-
- 
-
-// //Route to submit login info
-// app.get('/submit', (req, res) => {
-//    // the statement below assigns the paramters passed from the from via the name attribute to the variable formInfo.  
-//    var formInfo = req.query;
-  
-//    // the second argument passes data back to the 
-//    res.render('user', {name : formInfo.firstName, lastname : formInfo.lastName})
-// })
-
-
-// app.post('/submit', (req, res) => {
-//   const enteredUsername = req.body.username;
-//   const enteredPassword = req.body.password; // Assuming password is also sent in the request body
-
-//   // Call the function to check if the user exists in the database
-//   dbOperations.getUserByUserName(enteredUsername, enteredPassword, (err, user) => {
-//       if (err) {
-//           // Handle database error
-//           console.error('Database error:', err);
-//           res.status(500).send('Internal Server Error');
-//           return;
-//       }
-
-//       if (user.message) {
-//           // User not found or incorrect password
-//           res.render('userNotFound.hbs');
-//           return;
-//       }
-
-//       // User found, render the welcome message
-//       res.render('user.hbs', { name : formInfo.firstName, lastName : formInfo.lastName });
-//   });
-// });
-
-
-// Route to submit
+// Route #1
+// Renders data back to the client (Welcome Firstname, Lastname) (1)
+// submit login details
 app.get('/submit', async(req, res) => {
-  console.log("query: ", req.query)
   dbOperations.getUserByUserName(req.query.Uname, req.query.password, res)
-  //  console.log("user: ", user)
-  //  if (!user) {
-  //     res.render('userNotFound', {})
-  //  }
 })
- 
 
-
- // Route to get all books
- app.get('/books', async (req, res) => {
+// Route #2
+// Renders data back to the client (Book details) (2)
+// Makes call to external REST Api
+// Returns JSON Data from external REST Api
+// Route to get all books
+app.get('/books', async (req, res) => {
    try {
-
-     const response = await axios.get('https://openlibrary.org/api/books?limit=10'); // Make an API call to Open Library
-     console.log("books: ", response.data)
-
-     const books = response.data.docs;
+     const response = await axios.get('https://gutendex.com/books/'); // Make an API call to Open Library
+     const books = response.data.results;
      res.render('books.hbs', { books }); // Render the books template with the retrieved books
    } catch (error) {
      console.error('Error fetching books:', error);
      res.status(500).send('Error retrieving books');
    }
- });
+});
  
- // Route to get a specific book by ID
- app.get('/book/:id', async (req, res) => {
-   const bookId = req.params.id;
- 
-   try {
-     const response = await axios.get(`https://openlibrary.org}`); // Make an API call to Open Library
-     const book = response.data;
-     res.render('book.hbs', { BookHub }); // Render the book template with the retrieved book
-   } catch (error) {
-     console.error('Error fetching book:', error);
-     res.status(404).send('Book not found');
-   }
- });
- 
- // Route to add a new book review
- app.post('/review', (req, res) => {
-   const bookId = req.body.bookId;
-   const rating = req.body.rating;
-   const reviewText = req.body.reviewText;
- 
-   // Process review data
-   console.log(`Book ID: ${bookId}, Rating: ${rating}, Review Text: ${reviewText}`);
- 
-   res.send('Thank you for your review!');
- });
+// Route #3
+// Renders data back to the client (User details - Username, Firstname, Lastname, Email) (3)
+// routing to profile 
+app.get('/profileAccount', function (req,res){
+  res.render('profileAccount.hbs', {title:"Your Profile"})
+});
+
+// Route #4
+// Routing button updatemail
+app.get('/changeMail', function (req, res) {
+  res.render('changeMail.hbs', { title: "Update Your Email" });
+});
+
+// Route #5
+// Form to create new account
+app.get('/createAccount', function (req, res) {
+   res.render('createAccount.hbs', { title: "Create Account" });
+});
+
+// Route #5
+// Form to create new account
+app.post('/createUser', function (req, res) {
+  dbOperations.createUser(req.body.userName, req.body.password, req.body.firstName, req.body.lastName, req.body.email, res)
+});
+
+// Route #6
+// Success message after new account creation
+app.get('/accountCreated', function (req, res) {
+  res.render('accountCreated.hbs', { title: "Account Created Successfully! Welcome to BookHub!" });
+});
 
 
+app.get('/get_all_users', function (req, res) {
+    // Call the function to get all users
+    dbOperations.getAllUsers(res);
+});
+
+app.post('/delete_user', function (req, res) {
+   const {userName, password, firstName, lastName, email, userId} = req.query;
+   // Calling the 'deleteAccount'
+   dbOperations.deleteAccount(userName, password, firstName, lastName, email, userId, res);
+});
+
+app.get('/deleteAccount', function (req, res) {
+   res.render('deleteAccount.hbs', { title: "Account Created Successfully! Welcome to BookHub!" });
+});
 
 //routing  database
-
 app.get('/', function (req, res) {
 	res.render('index.hbs', {title: "BookHub"})
    
@@ -189,61 +109,7 @@ app.get('/', function (req, res) {
 
 });
 
-//routing to profile 
-   app.get('/profileAccount', function (req,res){
-   res.render('profileAccount.hbs', {title:"Your Profile"})
-   });
-      //Routing  button updatemail
-      app.get('/changeMail', function (req, res) {
-      res.render('changeMail.hbs', { title: "Update Your Email" });
-      });
 
-
-
-
-//routing to create account 
-
-app.get('/createAccount', function (req, res) {
-   res.render('createAccount.hbs', { title: "Create Account" });
-});
-
-   //Routing to AccountCreated
-
-   app.get('/accountCreated', function (req, res) {
-   res.render('accountCreated.hbs', { title: "Account Created Successfully! Welcome to BookHub!" });
-   });
-
-   //create account button 
-   app.post('/create_User', function (req, res) {
-   const {userName, password, firstName, lastName, email} = req.query;
-
-   dbOperations.createUser(userName, password, firstName, lastName, email);
-   
-   });
-
-
-
-// routing to get all users (show all the user on local:host3000/get_all_users)
-
-app.get('/get_all_users', function (req, res) {
-    // Call the function to get all users
-    dbOperations.getAllUsers(res);
-});
-
-
-
-//routing to delete
-
-app.post('/delete_user', function (req, res) {
- 
-   const {userName, password, firstName, lastName, email, userId} = req.query;
-   // Calling the 'deleteAccount'
-   dbOperations.deleteAccount(userName, password, firstName, lastName, email, userId, res);
- });
-
- app.get('/deleteAccount', function (req, res) {
-   res.render('deleteAccount.hbs', { title: "Account Created Successfully! Welcome to BookHub!" });
-});
 
 // listen port 
  app.listen(3000, () => {
